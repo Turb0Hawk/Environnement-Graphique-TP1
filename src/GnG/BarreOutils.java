9fe589101de885b7e02a3df072b3df6dd8cbb76e
David Ringuet <davidringuet500@gmail.com>
2019-02-21 17:25:49 -0500

    toolbar is done

--- src/GnG/BarreOutils.java
+++ src/GnG/BarreOutils.java
@@ -1,9 +1,15 @@
 package GnG;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 
@@ -11,44 +17,77 @@ public class BarreOutils extends JToolBar {
 	JButton btnOvale = new JButton();
 	JButton btnRectangle = new JButton();
 	JButton btnTrait = new JButton();
+	JToggleButton btnVide = new JToggleButton();
+
 	ImageIcon iconeOvale = new ImageIcon( BarreOutils.class.getResource( "Images\\ovale.png" ) );
 	ImageIcon iconeRectangle = new ImageIcon( BarreOutils.class.getResource( "Images\\rectangle.png" ) );
 	ImageIcon iconeTrait = new ImageIcon( BarreOutils.class.getResource( "Images\\trait.png" ) );
-	
-	JButton btnCouleur1 = new JButton();
-	JButton btnCouleur2 = new JButton();
-	JButton btnCouleur3 = new JButton();
-	JButton btnCouleur4 = new JButton();
-	JButton btnCouleur5 = new JButton();
-	JButton btnCouleur6 = new JButton();
+	ImageIcon iconeVide = new ImageIcon( BarreOutils.class.getResource( "Images\\none.png" ) );
+
+	JPanel panelRemplissage = new JPanel();
+	JPanel panelTrait = new JPanel();
+
+	ButtonGroup groupeRemplissage = new ButtonGroup();
+	ButtonGroup groupeTrait = new ButtonGroup();
+
+	JToggleButton[] btnsRemplissage = new JToggleButton[6];
+	JToggleButton[] btnsTrait = new JToggleButton[6];
+	String[] couleurs = { "#000000", "#ffcfea", "#feffbe", "#cbffe6", "#afe9ff", "#bfb9ff" };
+	String[] nomCouleurs = { "noir noirci", "rose songes d'été", "jaune canari", "vaporGreen TM",
+			"blue A E S T H E T I C", "fushia dépressif" };
+
 	public BarreOutils() {
 		super();
 		setFloatable( false );
 		setOrientation( SwingConstants.HORIZONTAL );
+
 		btnOvale.setIcon( iconeOvale );
+		btnOvale.setToolTipText( "Outil pour dessiner des Ovales et des cercles" );
+
 		btnRectangle.setIcon( iconeRectangle );
+		btnRectangle.setToolTipText( "Outil pour dessiner des rectangles et des carrés" );
+
 		btnTrait.setIcon( iconeTrait );
-		btnCouleur1.setBackground( Color.decode( "#ffcfea" ) );
-		btnCouleur2.setBackground( Color.decode( "#feffbe" ) );
-		btnCouleur3.setBackground( Color.decode( "#cbffe6" ) );
-		btnCouleur4.setBackground( Color.decode( "#afe9ff" ) );
-		btnCouleur5.setBackground( Color.decode( "#bfb9ff" ) );
-		btnCouleur6.setBackground( Color.BLACK);
-		btnCouleur1.setSize( 32, 32 );
-		btnCouleur2.setSize( 32, 32 );
-		btnCouleur3.setSize( 32, 32 );
-		btnCouleur4.setSize( 32, 32 );
-		btnCouleur5.setSize( 32, 32 );
-		btnCouleur6.setSize( 32, 32 );
+		btnTrait.setToolTipText( "Outil pour dessiner des traits" );
+
+		btnVide.setIcon( iconeVide );
+		btnVide.setToolTipText( "Aucun Remplissage" );
+		btnVide.setPreferredSize( new Dimension( 32, 32 ) );
+
+		panelRemplissage.add( new JLabel( "Remplissage" ) );
+		panelRemplissage.setSize( new Dimension( 300, 40 ) );
+
+		panelTrait.add( new JLabel( "Trait" ) );
+		panelTrait.setSize( new Dimension( 300, 40 ) );
+
+		for ( int i = 0; i < couleurs.length; ++i ) {
+			btnsRemplissage[i] = new JToggleButton();
+			btnsRemplissage[i].setToolTipText( nomCouleurs[i] );
+			btnsRemplissage[i].setBackground( Color.decode( couleurs[i] ) );
+			btnsRemplissage[i].setPreferredSize( new Dimension( 32, 32 ) );
+			groupeRemplissage.add( btnsRemplissage[i] );
+			panelRemplissage.add( btnsRemplissage[i] );
+
+			btnsTrait[i] = new JToggleButton();
+			btnsTrait[i].setToolTipText( nomCouleurs[i] );
+			btnsTrait[i].setBackground( Color.decode( couleurs[i] ) );
+			btnsTrait[i].setPreferredSize( new Dimension( 32, 32 ) );
+			groupeTrait.add( btnsTrait[i] );
+			panelTrait.add( btnsTrait[i] );
+		}
+		groupeRemplissage.add( btnVide );
+		panelRemplissage.add( btnVide );
+
+		btnsRemplissage[1].setSelected( true );
+		btnsTrait[1].setSelected( true );
+
 		add( btnTrait );
 		add( btnRectangle );
 		add( btnOvale );
 		addSeparator();
-		add( btnCouleur1 );
-		add( btnCouleur2 );
-		add( btnCouleur3 );
-		add( btnCouleur4 );
-		add( btnCouleur5 );
-		add( btnCouleur6 );
+		add( panelTrait, BorderLayout.WEST );
+		addSeparator();
+		add( panelRemplissage, BorderLayout.WEST );
+
 	}
 }
